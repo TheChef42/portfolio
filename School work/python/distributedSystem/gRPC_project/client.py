373a03ef6934a521f29f4f97706324c31847b614
TheChef42 <danielflute@gmail.com>
2023-10-01 12:13:29 +0200

    Done, a program that takes N files and creates N threads, which uses gRPC to communicate with a server to calculate the occurence of each word across alle files.

--- School work/python/distributedSystem/gRPC_project/client.py
+++ School work/python/distributedSystem/gRPC_project/client.py
@@ -1,57 +1,58 @@
-from concurrent import futures
 import os, glob, threading, grpc, gRPC_pb2_grpc, gRPC_pb2, logging
 
 
-def Calculate(stub, filename, results):
+def Calculate(stub, filename):  # a gRPC function that calls the calculate function on the server
         with open(os.path.join(os.getcwd(), filename), 'r') as f:
             text = gRPC_pb2.TextToCount(text=str(f.read()))
-            response = stub.Calculate(text)
-            count = response.WordOccurrence
+            response = stub.Calculate(text)  # calling the calculate function on the server
+            count = response.WordOccurrence  # extracting the WordOccurrence from the response
             return list(count.items())
 
 
-def Combine(stub, allwords):
+def Combine(stub, allwords):  # a gRPC function that calls the combine function on the server
     grpclist = []
-    for x in allwords:
+    for x in allwords:  # converts the dictionary to a list of the gRPC structure called tuples
         grpclist.append(gRPC_pb2.tuples(key=x[0], value=x[1]))
-    responecombine = stub.Combine(gRPC_pb2.TextToCombine(allwords=grpclist))
-    return dict(responecombine.WordOccurrence)
+    responecombine = stub.Combine(gRPC_pb2.TextToCombine(allwords=grpclist))  # calling the combine function on the server
+    return dict(responecombine.WordOccurrence)  # extracting the WordOccurence from response and converting it to a dictionary
 
 
-def worker(threadname, filenames, results, channel, lock):
-    stub = gRPC_pb2_grpc.FrequencyCalculatorStub(channel)
+def worker(threadname, filenames, results, stub, lock):  # a worker function that defines what each thread should do
     thread_results = []
 
-    for filename in filenames:
-        thread_results = Calculate(stub, filename, thread_results) + thread_results
+    for filename in filenames:  # filenames is a list, so the loop extracts the filename
+        thread_results = Calculate(stub, filename) + thread_results  # call the calculate function
 
-    with lock:
+    with lock:  # preventing race conditions when modifying the result list
         results.extend(list(thread_results))
 
 def run():
-    # NOTE(gRPC Python Team): .close() is possible on a channel and should be
-    # used in circumstances in which the with statement does not fit the needs
-    # of the code.
     allwords = []
     with (grpc.insecure_channel("localhost:50052") as channel):
-        n = len(glob.glob('input/*.txt')) # sets n to the amount of .txt files in the sub folder input
+        n = len(glob.glob('input/*.txt'))  # sets n to the amount of .txt files in the sub folder input
         filenames = glob.glob('input/*.txt')
         threads = []
-        lock = threading.Lock()
+        lock = threading.Lock()  # creates a lock, to be used in the worker function
+        stub = gRPC_pb2_grpc.FrequencyCalculatorStub(channel)
+
+        # creating a thread for each file
         for i in range(n):
-            thread = threading.Thread(name=str(i), target=worker, args=(i, filenames[i::n], allwords, channel, lock))
+            thread = threading.Thread(name=str(i), target=worker, args=(i, filenames[i::n], allwords, stub, lock))
             threads.append(thread)
 
+        # running all the threads
         for thread in threads:
             thread.start()
 
+        # waiting for all the threads to finish
         for thread in threads:
             thread.join()
 
-        stub = gRPC_pb2_grpc.FrequencyCalculatorStub(channel)
+        # when all threads are done I call the Combine function and overwrite the allwords with the result
         allwords = Combine(stub, allwords)
-        allwords = dict(sorted(allwords.items(), key=lambda x: x[1], reverse=True))
-        print(allwords)
+        allwords = dict(sorted(allwords.items(), key=lambda x: x[1], reverse=True))  # here I sort the result
+        print("freq = " + str(allwords))
+        channel.close()
 
 
 if __name__ == '__main__':
--- School work/python/distributedSystem/gRPC_project/server.py
+++ School work/python/distributedSystem/gRPC_project/server.py
@@ -1,26 +1,29 @@
 from concurrent import futures
 import grpc, gRPC_pb2_grpc, gRPC_pb2, logging
 
-# Press the green button in the gutter to run the script.
-
 
 class FrequencyCalculatorServicer(gRPC_pb2_grpc.FrequencyCalculatorServicer):
 
+
     def Calculate(self, request, context):
         counts = dict()
-        words = request.text.split()
+        words = request.text.split()  # splits the recived text into words in a list
 
-        for word in words:
+        for word in words:  # looks at each word and sees if it is in the dictionary counts, if yes adds one to the value
+            word = ''.join(letter for letter in word if letter.isalnum())  # sanitizing each word and removing symbols
             if word in counts:
                 counts[word] += 1
             else:
                 counts[word] = 1
-        counts = dict(sorted(counts.items(), key=lambda x: x[1], reverse=True))
         count = gRPC_pb2.WordCount(WordOccurrence=counts)
         return count
 
+
     def Combine(self, request, context):
         totalcount = {}
+        # runs over the list containing the message structure tuples
+        # they have a key(string, the word) and a value(int64 the occurence of the word)
+        # it checks if the dict totalcount has the word if yes then adds the value of the new word (can be duplicate)
         for tuple_item in request.allwords:
             key = tuple_item.key
             value = tuple_item.value
@@ -31,8 +34,7 @@ class FrequencyCalculatorServicer(gRPC_pb2_grpc.FrequencyCalculatorServicer):
         return gRPC_pb2.TotalCount(WordOccurrence=totalcount)
 
 
-
-def serve():
+def serve():  # creates a server that can handle multiple threads
     server = grpc.server(futures.ThreadPoolExecutor())
     gRPC_pb2_grpc.add_FrequencyCalculatorServicer_to_server(
         FrequencyCalculatorServicer(), server
@@ -41,6 +43,7 @@ def serve():
     server.start()
     print("server running")
     server.wait_for_termination()
+
 
 if __name__ == '__main__':
     logging.basicConfig()
